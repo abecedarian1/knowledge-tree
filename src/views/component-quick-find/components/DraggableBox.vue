c69d97c6d49769b01d776c171c84c78fb11f9bea
abecedarian1 <1584968548@qq.com>
2024-06-23 23:49:58 +0800

    update

--- src/views/component-quick-find/components/DraggableBox.vue
+++ src/views/component-quick-find/components/DraggableBox.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="container">
-        <div class="dropzone"  @drop="drop" @dragstart="dragstart(true)" @dragend="dragend" @dragenter="dragenter" @dragleave="dragleave">
+    <!-- 事件委托 -->
+    <div class="container" 
+        @drop="drop" 
+        @dragstart="dragstart"  
+        @dragend="dragend" 
+        @dragenter="dragenter" 
+        @dragleave="dragleave"
+        @dragover="dragover"
+    >
+        <div class="dropzone origin">
             <div class="drag-box" draggable="true">
                 <span class="delete-icon" @click="deleteNode(false)">X</span>
                 可拖拽框
             </div>
         </div>
-
-        <div class="dropzone" @drop="drop"  @dragstart="dragstart(false)" @dragend="dragend" @dragover="dragover" @dragenter="dragenter" @dragleave="dragleave">
-        </div>
-
-        <div class="dropzone" @drop="drop"  @dragstart="dragstart(false)" @dragend="dragend" @dragover="dragover" @dragenter="dragenter" @dragleave="dragleave">
-        </div>
-
-        <div class="dropzone" @drop="drop"  @dragstart="dragstart(false)" @dragend="dragend" @dragover="dragover" @dragenter="dragenter" @dragleave="dragleave">
-        </div>
-
-        <div class="dropzone" @drop="drop"  @dragstart="dragstart(false)" @dragend="dragend" @dragover="dragover" @dragenter="dragenter" @dragleave="dragleave">
-        </div>
+        <div class="dropzone"></div>
+        <div class="dropzone"></div>
+        <div class="dropzone"> </div>
+        <div class="dropzone"></div>
     </div>
 </template>
 
@@ -28,17 +28,17 @@
     const drag = ref();
     const dragCopy = ref();
 
-
     //开始移动了
-    const dragstart=(boolean)=>{
-        if(event.target.className == 'drag-box'){
-            if(boolean){
-                drag.value = event.target
+    const dragstart=(e)=>{
+        if(e.target.className == 'drag-box'){
+            //源节点
+            if(e.target.parentNode.className.includes('origin')){
+                drag.value = e.target
             }else{
-                dragCopy.value = event.target
+                dragCopy.value = e.target
             }
             //拖拉过程中，被拖拉元素的状态
-            event.target.style.backgroundColor='rgb(152, 166, 226,0.5)';
+            e.target.style.backgroundColor='rgb(152, 166, 226,0.5)';
         }
     }
     
@@ -56,13 +56,15 @@
     //连续触发
     //设置允许放置到当前节点的条件--使得drop事件起作用；同时防止拖拉效果被重置
     const dragover=(e)=>{
-        if((drag.value|| dragCopy.value) && e.target.childNodes.length==0){
-            e.preventDefault();
+        //源上不放
+        if(!e.target.className.includes('origin') && e.target.className=='dropzone'){
+            if((drag.value|| dragCopy.value) && e.target.childNodes.length==0){
+                e.preventDefault();
+            }
         }
     }
 
     //进入当前节点了
-    //该事件会冒泡====
     const dragenter=(e)=>{
         //只有拖动允许拖拉的元素才会触发
         if(e.target.className=='dropzone' && (drag.value || dragCopy.value)){
@@ -72,28 +74,32 @@
 
     //从当前节点离开了
     const dragleave=(e)=>{
-        //只有允许拖拉的元素离开才会触发
-        if(drag.value || dragCopy.value){
-            e.target.style.backgroundColor = ''
+        if(e.target.className == 'dropzone'){
+            //只有允许拖拉的元素离开才会触发
+            if(drag.value || dragCopy.value){
+                e.target.style.backgroundColor = ''
+            }
         }
     }
 
     //向当前节点放拖拉对象
     const drop=(e)=>{
-        if(e.target.childNodes.length==0){
-            if(drag.value){
-                let cloneNode = drag.value.cloneNode(true)
-                let eventNode = cloneNode.children[0]
-                eventNode.style.display = 'inline'
-                eventNode.addEventListener('click',()=>{deleteNode(true)},false);
-                e.target.appendChild(cloneNode)
-                cloneNode.style.backgroundColor='rgb(152, 166, 226)';     
-            }else if(dragCopy.value){
-                dragCopy.value.parentNode.removeChild(dragCopy.value);
-                e.target.appendChild(dragCopy.value)
-                dragCopy.value.style.backgroundColor='rgb(152, 166, 226)';     
+        if(e.target.className == 'dropzone'){
+            if(e.target.childNodes.length==0){
+                if(drag.value){
+                    let cloneNode = drag.value.cloneNode(true)
+                    let eventNode = cloneNode.children[0]
+                    eventNode.style.display = 'inline'
+                    eventNode.addEventListener('click',()=>{deleteNode(true)},false);
+                    e.target.appendChild(cloneNode)
+                    cloneNode.style.backgroundColor='rgb(152, 166, 226)';     
+                }else if(dragCopy.value){
+                    dragCopy.value.parentNode.removeChild(dragCopy.value);
+                    e.target.appendChild(dragCopy.value)
+                    dragCopy.value.style.backgroundColor='rgb(152, 166, 226)';     
+                }
+                e.target.style.backgroundColor = ''
             }
-            e.target.style.backgroundColor = ''
         }
     }
 
